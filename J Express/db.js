72d1e5314085f0ac305159141ec660bde1381444
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2025-01-13 19:42:10 +0530

    Add files via upload

---
+++ J Express/db.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2');
+const db = mysql.createPool({
+  host: 'localhost',     
+  user: 'root',          
+  password: '',  
+  database: 'resume_data',  
+});
+
+module.exports = db;
