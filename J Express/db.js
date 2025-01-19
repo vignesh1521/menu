9db2713540468a2cc1c1bfb94549497af9a0838b
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2025-01-19 09:05:39 +0530

    Delete J Express directory

--- J Express/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2');
-const db = mysql.createPool({
-  host: 'localhost',     
-  user: 'root',          
-  password: '',  
-  database: 'resume_data',  
-});
-
-module.exports = db;
